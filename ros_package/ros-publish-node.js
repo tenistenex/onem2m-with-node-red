9853eaf453da27000f7593cfec821c3fba26fac7
tenisten <tenisten@livemail.tw>
2018-04-16 11:25:34 +0800

    add ros-with-node-red

---
+++ ros_package/ros-publish-node.js
@@ -0,0 +1,35 @@
+module.exports = function(RED) {
+  return function (config) {
+    var ROSLIB = require('roslib'); 
+
+    RED.nodes.createNode(this,config);
+    var node = this;
+
+    node.server = RED.nodes.getNode(config.server);
+    
+    if (!node.server || !node.server.ros){
+      return;
+    }
+
+    var topic = new ROSLIB.Topic({
+      name : config.topicname,
+      messageType : config.msgtype
+    });
+
+    node.on('input', (msg) => {
+      topic.ros = node.server.ros;
+      node.log('publishing msg ' + msg.payload);
+      // var pubslishMsg = new ROSLIB.Message({data: msg.payload});
+      topic.publish(msg.payload);
+    });
+
+    node.server.on('ros connected', () => {
+      node.status({fill:"green",shape:"dot",text:"connected"});
+    });
+
+    node.server.on('ros error', () => {
+      node.status({fill:"red",shape:"dot",text:"error"});
+    });
+
+  }
+}
